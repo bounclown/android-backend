b00e9ebed77089b840f3d35a1941ab1b2cb0d241
Nikita <nikita71982006@mail.ru>
2026-02-11 18:53:57 +0700

    На всякий случай, чтобы если что восстановить, хочу полностью переработать структуру проекта

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <thread>
 #include <cmath>
 
+#include <zmq.hpp>
+#include <nlohmann/json.hpp>
+#include <zmq_addon.hpp>
 #include "backends/imgui_impl_opengl3.h"
 #include "backends/imgui_impl_sdl2.h"
 #include "imgui.h"
@@ -20,7 +23,7 @@ struct location {
     float latitude;
     float longitude;
     float altitude;
-    // и т.д.
+    
 };
 
 void run_gui(location *loc) {
@@ -29,6 +32,17 @@ void run_gui(location *loc) {
 }
 
 void run_server(location *loc) {
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, ZMQ_REP);
+    socket.bind("tcp://*:5555");
+    
+    for (;;) {
+        zmq::message_t request;
+        socket.recv(request);
+
+
+
+    }
     // Здесь работает поток серверный
     // Записывает данные в ГЛОБАЛЬНУЮ структуру location
 }
@@ -36,6 +50,14 @@ void run_server(location *loc) {
 int main(int argc, char* argv[]){
     static location locationInfo;
 
+    try {
+        zmq::context_t context(1);
+        zmq::socket_t socket(context, zmq::socket_type::rep);
+        std::cout << "ZMQ Context created successfully!" << std::endl;
+    } catch (const zmq::error_t& e) {
+        std::cerr << "ZMQ Error: " << e.what() << std::endl;
+    }
+    
     std::thread gui_thread(run_gui, &locationInfo);
     std::thread server_thread(run_server, &locationInfo);
 
