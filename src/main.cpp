888d5384671c387e7fd13370d2b6ffc17951f6ae
Nikita <nikita71982006@mail.ru>
2026-02-18 19:53:08 +0700

    дописал функцию run_server

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <thread>
 #include <cmath>
+#include <fstream>
+#include <iomanip>
 
 #include <zmq.hpp>
 #include <nlohmann/json.hpp>
@@ -14,6 +16,7 @@
 #include "backends/imgui_impl_sdl2.h"
 #include "imgui.h"
 #include "implot.h"
+
 
 extern "C" int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
     return main(__argc, __argv);
@@ -34,30 +37,46 @@ void run_gui(location *loc) {
 void run_server(location *loc) {
     zmq::context_t context(1);
     zmq::socket_t socket(context, ZMQ_REP);
-    socket.bind("tcp://*:5555");
+
+    try {
+        socket.bind("tcp://*:5555");
+        std::cout << "|Сервер| прослушивание на порту 5555" << std::endl;
+    } catch(const zmq::error_t& e) {
+        std::cerr << "|Сервер| ошибочка" << e.what() << std::endl;
+        return;
+    }
+    
     
     for (;;) {
         zmq::message_t request;
-        socket.recv(request);
+        auto res = socket.recv(request, zmq::recv_flags::none);
+        if (!res) continue;
 
+        try {
+            std::string msg_str(static_cast<char*>(request.data()), request.size());
+            auto j = nlohmann::json::parse(msg_str);
 
+            loc->latitude = j.value("lat", 0.0f);
+            loc->longitude = j.value("lon", 0.0f);
+            loc->altitude = j.value("alt", 0.0f);
 
+            std::ofstream file("last_location.json");
+            if (file.is_open()) {
+                file << std::setw(4) << j << std::endl;
+                file.close();
+            }
+            socket.send(zmq::str_buffer("ACK"), zmq::send_flags::none);
+
+        } catch (const std::exception& e) {
+            std::cerr << "[Сервер] ошибка с файлом: " << e.what() << std::endl;
+            socket.send(zmq::str_buffer("ERROR"), zmq::send_flags::none);
+        }
     }
-    // Здесь работает поток серверный
-    // Записывает данные в ГЛОБАЛЬНУЮ структуру location
 }
 
 int main(int argc, char* argv[]){
     static location locationInfo;
 
-    try {
-        zmq::context_t context(1);
-        zmq::socket_t socket(context, zmq::socket_type::rep);
-        std::cout << "ZMQ Context created successfully!" << std::endl;
-    } catch (const zmq::error_t& e) {
-        std::cerr << "ZMQ Error: " << e.what() << std::endl;
-    }
-    
     std::thread gui_thread(run_gui, &locationInfo);
     std::thread server_thread(run_server, &locationInfo);
 
